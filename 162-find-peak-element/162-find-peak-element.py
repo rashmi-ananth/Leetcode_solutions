665afac727a4b6176c2514fde749ef4915f92924
Rashmi Ananth <66443393+rashmi-ananth@users.noreply.github.com>
2022-08-23 12:43:10 -0700

    Time: 52 ms (86.81%), Space: 14.1 MB (10.03%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.py
+++ 162-find-peak-element/162-find-peak-element.py
@@ -4,19 +4,16 @@ class Solution:
         if len(nums) == 1:
             return 0
         
-        
-        
         length = len(nums)
         def findPeak(l, r, nums):
             
             mid = (l + r) // 2
             val = nums[mid]
             
-            
             if ((mid + 1 < length and nums[mid + 1] < val) or mid + 1 >= length) and ((mid - 1 >= 0 and nums[mid - 1] < val) or mid - 1 < 0):
                 return mid
             
-            if mid + 1 < length and mid - 1 >= 0:
+            if (mid + 1 < length and mid - 1 >= 0) or (mid + 1 < length):
                 if nums[mid + 1] > nums[mid - 1]:
                     return findPeak(mid + 1, r, nums)
                 else:
