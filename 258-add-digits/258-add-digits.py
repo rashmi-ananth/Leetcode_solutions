3636098bce8d3f57538d4884ec25ff88252f0cb4
Rashmi Ananth <66443393+rashmi-ananth@users.noreply.github.com>
2022-09-08 19:15:17 -0400

    Time: 65 ms (17.88%), Space: 13.9 MB (9.59%) - LeetHub

--- 258-add-digits/258-add-digits.py
+++ 258-add-digits/258-add-digits.py
@@ -1,23 +1,14 @@
 class Solution:
     def addDigits(self, num: int) -> int:
         
-        stack = []
-        # print(38 % 10)
+        if num == 0:
+            return 0
+        if num % 9 == 0:
+            return 9
         
-        while num != 0:
-            stack.append(num % 10)
-            num = num // 10
-        
-        total = 0
-        while stack:
-            num += stack.pop()
-            if len(stack) == 0 and num > 9:
-                while num != 0:
-                    stack.append(num % 10)
-                    num = num // 10
                     
 
-        return num
+        return num % 9
             
         
         
