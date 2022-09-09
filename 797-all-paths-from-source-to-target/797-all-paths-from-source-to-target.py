af152559cb66ce785c5afc6642bffe37decd9aff
Rashmi Ananth <66443393+rashmi-ananth@users.noreply.github.com>
2022-09-09 17:22:38 -0400

    Time: 224 ms (7.20%), Space: 15.6 MB (79.15%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.py
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.py
@@ -6,17 +6,17 @@ class Solution:
         target = len(graph) - 1
         
         return_lst = []
-        def dfs(source, path):
+        def dfs(source, path, graph):
             
             if source == target:
                 return_lst.append(path)
                 return
             
             for i in range(len(graph[source])):
-                dfs(graph[source][i], path + [graph[source][i]])
+                dfs(graph[source][i], path + [graph[source][i]], graph)
             
             
-        dfs(source, [source])
+        dfs(source, [source], graph)
         
         return return_lst
         
