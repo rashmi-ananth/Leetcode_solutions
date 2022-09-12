c1a30be9666aeaba8ccad635a657b9260b6456ca
Rashmi Ananth <66443393+rashmi-ananth@users.noreply.github.com>
2022-09-12 16:15:58 -0400

    Time: 162 ms (25.54%), Space: 14.1 MB (100.00%) - LeetHub

--- 1244-design-a-leaderboard/1244-design-a-leaderboard.py
+++ 1244-design-a-leaderboard/1244-design-a-leaderboard.py
@@ -4,24 +4,16 @@ class Leaderboard:
     def __init__(self):
         self.dictionary = defaultdict(int)
     
-
     def addScore(self, playerId: int, score: int) -> None:
         self.dictionary[playerId] += score
-        
-        
-
+ 
     def top(self, K: int) -> int:
    
         sorted_items = sorted(self.dictionary.items(), key=lambda x: -x[1])[:K]
         total = 0
-
         for i in range(K):
             total += sorted_items[i][1]
-        
-
         return total
-        
-        
 
     def reset(self, playerId: int) -> None:
         del self.dictionary[playerId]
