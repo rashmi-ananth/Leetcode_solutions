eac471893f871883a208df1b8b8cf97e1bab6d94
Rashmi Ananth <66443393+rashmi-ananth@users.noreply.github.com>
2022-09-12 19:47:12 -0400

    Time: 843 ms (5.01%), Space: 17 MB (11.78%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.py
+++ 1472-design-browser-history/1472-design-browser-history.py
@@ -14,7 +14,6 @@ class Node:
         self.val = val
         self.next = next
 
-# start<->homepage<->end
 class BrowserHistory:
 
     def __init__(self, homepage: str):
@@ -27,45 +26,30 @@ class BrowserHistory:
         self.start.next = homepage_node
         
         self.curr = homepage_node
-        
-        # print(self.start.val, self.start.next.val, self.start.next.next.val)
-        
+
     def visit(self, url: str) -> None:
         new_page = Node(self.curr, url, self.end)
         new_page.next.prev = new_page
         new_page.prev.next = new_page
         
         self.curr = new_page
-        
-#         print(self.curr.prev.val, self.curr.next.val, self.curr.val)
-        
+          
     def back(self, steps: int) -> str:
-#         print('back')
-#         print(steps)
-#         print(self.curr.prev.val, self.curr.val, self.curr.next.val)
-        
         
         while steps > 0:
             if self.curr.prev.val == "start":
                 break
             self.curr = self.curr.prev    
             steps -= 1
-            
-        # print(self.curr.val)
         
         return self.curr.val
 
     def forward(self, steps: int) -> str:
-        # print('forward')
-        # print(steps)
-        # print(self.curr.prev.val, self.curr.val, self.curr.next.val)
-        
         while steps > 0:
             if self.curr.next.val == "end":
                 break
             self.curr = self.curr.next    
             steps -= 1
-        # print(self.curr.val)
         return self.curr.val
 
 
