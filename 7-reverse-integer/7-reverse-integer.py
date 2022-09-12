4f8d486d63856a3cf9c8173ab95f041aa6f188c5
Rashmi Ananth <66443393+rashmi-ananth@users.noreply.github.com>
2022-09-11 21:46:15 -0400

    Time: 57 ms (38.87%), Space: 13.8 MB (64.42%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.py
@@ -0,0 +1,37 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        
+        
+        # check if negative
+        check = False
+        if x < 0:
+            check = True
+            
+        # 123
+        # 0
+        
+        new_int = 0
+        factor = 10
+        x = abs(x)
+        find_nonzero = False
+        
+        while x > 0:
+            right = x % 10
+            if right == 0 and not find_nonzero:
+                x //= 10
+                continue
+            find_nonzero = True
+            new_int = (new_int * factor) + right
+            x //= 10
+
+        if check:
+            new_int = -new_int
+        
+        if new_int > (2**31 - 1) or new_int < -2**31:
+            return 0
+        
+        return new_int
+
+    
+    
+        
