1160889bd067083f0dc533dd59873164d0fbbafe
Rashmi Ananth <66443393+rashmi-ananth@users.noreply.github.com>
2022-09-13 12:47:15 -0400

    Time: 292 ms (12.71%), Space: 14.7 MB (54.38%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -4,9 +4,6 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         
-        for i in range(len(matrix)):
-            print(matrix[i])
-        
         col = False
         row = False
         for i in range(len(matrix[0])):
@@ -22,24 +19,15 @@ class Solution:
                     matrix[i][0] = 0
                     matrix[0][j] = 0
 
-        for i in range(len(matrix)):
-            print(matrix[i])
-        
         for i in range(1, len(matrix)):
             for j in range(1, len(matrix[0])):
-                
                 if matrix[i][0] == 0 or matrix[0][j] == 0:
                     matrix[i][j] = 0
-          
-        for i in range(len(matrix)):
-            print(matrix[i])
 
-        print(row)
+
         if row:
             for i in range(len(matrix[0])):
                 matrix[0][i] = 0
-         
-        print(col)
         if col:
             for j in range(len(matrix)):
                 matrix[j][0] = 0
