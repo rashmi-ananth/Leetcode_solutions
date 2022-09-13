a1517497dd6aad98ea70088a07425b61b76feb9d
Rashmi Ananth <66443393+rashmi-ananth@users.noreply.github.com>
2022-09-13 11:03:43 -0400

    Time: 66 ms (13.88%), Space: 13.9 MB (21.28%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.py
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.py
@@ -4,20 +4,21 @@ class Solution:
         if s1 == s2:
             return True
         
+        if sorted(s1) != sorted(s2):
+            return False
+        
         counter = 0
-        difference1 = set()
-        difference2 = set()
+
         for i in range(len(s1)):
             if s1[i] != s2[i]:
-                difference1.add(s1[i])
-                difference2.add(s2[i])
                 counter += 1
-                
+        return counter == 2 or counter == 0       
           
-        for i in list(difference1):
-            if i not in difference2:
-                return False
+        
+#         for i in list(difference1):
+#             if i not in difference2:
+#                 return False
 
-        return counter == 2 and len(difference1) == 2 and len(difference2) == 2
+#         return counter == 2 and len(difference1) == 2 and len(difference2) == 2
             
         
