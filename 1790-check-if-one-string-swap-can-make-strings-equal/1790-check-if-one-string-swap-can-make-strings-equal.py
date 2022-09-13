c5ca36a25e7739b5b78813bd8a83cd9f90a789a2
Rashmi Ananth <66443393+rashmi-ananth@users.noreply.github.com>
2022-09-13 11:03:49 -0400

    Time: 50 ms (51.59%), Space: 13.9 MB (21.28%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.py
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.py
@@ -14,11 +14,3 @@ class Solution:
                 counter += 1
         return counter == 2 or counter == 0       
           
-        
-#         for i in list(difference1):
-#             if i not in difference2:
-#                 return False
-
-#         return counter == 2 and len(difference1) == 2 and len(difference2) == 2
-            
-        
