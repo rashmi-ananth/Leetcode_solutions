dfe23b750f500b88bb1ea29cd11e3e0fd9232d95
Rashmi Ananth <66443393+rashmi-ananth@users.noreply.github.com>
2022-09-15 13:45:12 -0400

    Time: 55 ms (46.91%), Space: 14.1 MB (10.07%) - LeetHub

--- 289-game-of-life/289-game-of-life.py
+++ 289-game-of-life/289-game-of-life.py
@@ -3,25 +3,10 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-#         < 2 dies
-#         2-3 lives
-#         > 3 dies
-        
-#         3 live bute dead: live
-            
-#             0-1: -2
-#             1-0: -1
-
-
-
         for i in range(len(board)):
             for j in range(len(board[0])):
                 alive_n = 0
                 dead_n = 0
-                
-                # i > 0 j > 0
-#                 i < len(board) -1 j < len(board[0]) - 1
-                
                 if i < len(board) - 1:
                     val = board[i+1][j]
                     if val == 0 or val == -2:
@@ -87,22 +72,7 @@ class Solution:
                 if board[i][j] == -2:
                     board[i][j] = 1
                 
-                    
-                
-#                 i+1, j
-#                 i-1, j
-#                 i, j+1
-#                 i, j-1
-                
-#                 i+1, j+1
-#                 i-1, j-1
-#                 i+1, j-1
-#                 i-1, j+1
-                
-                
-                
-                
-                
+ 
                 
                 
                 
