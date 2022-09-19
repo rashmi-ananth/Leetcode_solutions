9e5d8725a9c7307b593ef5e80d1a317b3fe614ac
Rashmi Ananth <66443393+rashmi-ananth@users.noreply.github.com>
2022-09-19 12:14:23 -0400

    Time: 80 ms (29.51%), Space: 14.3 MB (55.49%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -1,17 +1,6 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         
-        
-        
-        
-        
-        
-        
-        0,1,2,3,4,5,6,7
-        7,0,1,2,3,4,5,6
-        4,5,6,7,0,1,2,3
-        3,4,5,6,7,8,0,1,2
-        
         l=0
         r=len(nums)-1
         
