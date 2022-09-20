69037f0e51087d1b3b7d60aaecbfea9b2c155be5
Rashmi Ananth <66443393+rashmi-ananth@users.noreply.github.com>
2022-09-20 15:34:01 -0400

    Time: 107 ms (30.43%), Space: 14.7 MB (27.74%) - LeetHub

--- 1553-minimum-number-of-days-to-eat-n-oranges/1553-minimum-number-of-days-to-eat-n-oranges.py
+++ 1553-minimum-number-of-days-to-eat-n-oranges/1553-minimum-number-of-days-to-eat-n-oranges.py
@@ -2,25 +2,34 @@ class Solution:
     def minDays(self, n: int) -> int:
         
         
-        min_days = dict()
-        def dfs(n, min_days):
-  
+        def backtrack(n):
+            
             if n == 1:
+                memo[1] = 1
                 return 1
+            if n == 2:
+                memo[2] = 2
+                return 2
+            if n == 3:
+                memo[3] = 2
+                return 2
             
-            if n == 0:
-                return 0
+            if n in memo.keys():
+                return memo[n]
             
-            if n in min_days.keys():
-                return min_days[n]
             
-            min_days[n] = 1 + min(n % 3 + dfs(n//3, min_days), n % 2 + dfs(n//2, min_days))
             
-            return min_days[n]
+            memo[n] = 1 + min((n%2) + backtrack(n//2), (n%3) + backtrack(n//3))
+            return memo[n]
+            
+            
+            
+        memo = dict()
+        backtrack(n)
+        
+        return memo[n]
         
         
-  
-        return dfs(n, min_days)
         
-        
-        
+
+            
