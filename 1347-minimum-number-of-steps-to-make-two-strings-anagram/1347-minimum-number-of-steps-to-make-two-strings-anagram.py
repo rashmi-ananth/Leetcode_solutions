b6e1a7e1060acbb34655ed308a2f70dd327f5679
Rashmi Ananth <66443393+rashmi-ananth@users.noreply.github.com>
2022-09-21 15:27:32 -0400

    Time: 876 ms (5.03%), Space: 14.6 MB (77.95%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -1,6 +1,7 @@
 class Solution:
     def minSteps(self, s: str, t: str) -> int:
         
+        # O(s)
         
         s_dict = defaultdict(int)
         t_dict = defaultdict(int)
@@ -18,35 +19,6 @@ class Solution:
                     count += s_dict[char] - t_dict[char]
         return count
                     
-                
-        
-        abb
-        aab
-        
-        
-        
-#         aab
-#         abb
-        
-#         a:2 b:1
-#         a:1 b:2
-        
-        
-#     l:1 e:3 t:1 c:1 o:1 d:1
-                            
-#     p:1 r:1 a:1 c:2 t:1 i:1 e:1
-                                
-                                
-                                
-#     1 + 2 + 0 + 0 + 1 + 1
-    
-#     total = 0
-#     if s[key] not in t.keys:
-#         total += s[key]
-#     else:
-#         if s[key] > t[key]:
-#             total += s[key] - t[key]
-        
-        
+            
         
         
