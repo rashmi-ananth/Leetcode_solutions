b372d03738b4dc082a014d7d27cec372cb0a4924
Rashmi Ananth <66443393+rashmi-ananth@users.noreply.github.com>
2022-09-20 22:20:33 -0400

    Time: 2044 ms (17.08%), Space: 75.7 MB (48.88%) - LeetHub

--- 146-lru-cache/146-lru-cache.py
+++ 146-lru-cache/146-lru-cache.py
@@ -1,22 +1,21 @@
 class Node:
-    
-    def __init__(self, prev, key,val, next):
+    def __init__(self, prev, key, val, next):
         self.prev = prev
         self.key = key
         self.val = val
         self.next = next
+    
 
 class LRUCache:
-
-
     def __init__(self, capacity: int):
-        self.dictionary = defaultdict(int)
-        self.LRU = Node(None, "","start", None)
-        self.MRU = Node(None, "", "end", None)
+        self.LRU = Node(None, 0, "LRU", None)
+        self.MRU = Node(self.LRU, 0, "LRU", None)
         self.LRU.next = self.MRU
-        self.MRU.prev = self.LRU
-        self.capacity = capacity
+        
+        self.dictionary = dict()
+
         self.counter = 0
+        self.capacity = capacity
     
         
     def get(self, key: int) -> int:
@@ -34,18 +33,16 @@ class LRUCache:
             return self.dictionary[key].val
         else:
             return -1
-        
-
+    
     def add_node(self, node):
         temp = self.MRU.prev
         temp.next = node
         node.prev = temp
         node.next = self.MRU
-        self.MRU.prev = node    
+        self.MRU.prev = node   
 
     def put(self, key: int, value: int) -> None:
         
-        # if in dictionary
         if key in self.dictionary.keys():
             
             # update curr node val
@@ -58,38 +55,29 @@ class LRUCache:
             
             # add curr node to front
             self.add_node(node)
-            
-        # if NOT in dictionary
+        
+        # if key not in dict and c < c
+        elif self.counter < self.capacity:
+               
+            self.counter += 1   
+            node = Node(None, key, value, None)
+            self.add_node(node)
+            self.dictionary[key] = node 
         else:
-            # if exceeds limit
-            if self.counter == self.capacity:
-                
-                # remove LRU node
-                temp = self.LRU.next
-                self.LRU.next = temp.next
-                temp.next.prev = self.LRU
-                temp.next = None
-                temp.prev = None
-                del self.dictionary[temp.key]
-            else:
-                self.counter += 1
-                
-            # adding new node
-            new_node = Node(None, key, value, None)
-            self.add_node(new_node)
-            self.dictionary[key] = new_node
-            
-
-
-            
-                
-            
-
-                
+            temp = self.LRU.next
+            self.LRU.next = temp.next
+            temp.next.prev = self.LRU
+            del self.dictionary[temp.key]
             
         
-   
+            node = Node(None, key, value, None)
+            self.add_node(node)
+            self.dictionary[key] = node 
         
+
+            
+
+    
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
