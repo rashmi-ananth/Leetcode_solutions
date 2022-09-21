f86dc01e40d597d48960e692fb0fe6db74cedbea
Rashmi Ananth <66443393+rashmi-ananth@users.noreply.github.com>
2022-09-20 21:13:15 -0400

    Time: 193 ms (17.83%), Space: 14.3 MB (39.56%) - LeetHub

--- 1244-design-a-leaderboard/1244-design-a-leaderboard.py
+++ 1244-design-a-leaderboard/1244-design-a-leaderboard.py
@@ -4,31 +4,39 @@ class Leaderboard:
     def __init__(self):
         self.dictionary = defaultdict(int)
         
-    
-    def addScore(self, playerId: int, score: int) -> None:
-        self.dictionary[playerId] += score
- 
-    def top(self, K: int) -> int:
-        h = []
-        idx = 0
-        items_lst = list(self.dictionary.items())
-        for key, value in items_lst:
-            heappush(h, (value, key))
-            idx += 1
-            if idx == K:
-                break
-        while idx < len(items_lst):
-            if items_lst[idx][1] > h[0][0]:
-                heappop(h)
-                heappush(h, (items_lst[idx][1], items_lst[idx][0]))
-            idx += 1
 
+    def addScore(self, playerId: int, score: int) -> None:
+        if playerId in self.dictionary.keys():
+            self.dictionary[playerId] += score
+        else:
+            self.dictionary[playerId] = score
+            
+    def top(self, K: int) -> int:
+        
+        h = []
+        dictionary_items = list(self.dictionary.items())
+        i = 0
+        while i < K:
+            heappush(h, (dictionary_items[i][1], dictionary_items[i][0]))
+            i += 1
+            
+        while i < len(dictionary_items):
+            if dictionary_items[i][1] > h[0][0]:
+                heappop(h)
+                heappush(h, (dictionary_items[i][1], dictionary_items[i][0]))
+            i += 1
+         
+        
         total = 0
-        for i in range(K):
+        for i in range(len(h)):
             total += h[i][0]
         return total
+            
+    
 
     def reset(self, playerId: int) -> None:
+        
+        self.dictionary[playerId] = 0
         del self.dictionary[playerId]
         
 
