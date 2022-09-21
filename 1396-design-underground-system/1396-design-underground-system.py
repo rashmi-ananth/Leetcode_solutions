7d6d6c0df8118cb1b821b03632d10a3a48603748
Rashmi Ananth <66443393+rashmi-ananth@users.noreply.github.com>
2022-09-21 18:35:05 -0400

    Time: 625 ms (10.51%), Space: 25.5 MB (20.33%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.py
+++ 1396-design-underground-system/1396-design-underground-system.py
@@ -13,7 +13,8 @@ class UndergroundSystem:
         start_station = self.check_in_dict[id][0]
         total_time = t - self.check_in_dict[id][1]
         if (start_station, stationName) in self.trips_dict.keys():
-            self.trips_dict[(start_station, stationName)] = (self.trips_dict[(start_station, stationName)][0] + total_time, 1 + self.trips_dict[(start_station, stationName)][1])
+            new_tuple = (self.trips_dict[(start_station, stationName)][0] + total_time, 1 + self.trips_dict[(start_station, stationName)][1])
+            self.trips_dict[(start_station, stationName)] = new_tuple
             
         else:
             self.trips_dict[(start_station, stationName)] = (total_time, 1)
@@ -24,27 +25,6 @@ class UndergroundSystem:
         
         avg = self.trips_dict[(startStation, endStation)][0] / self.trips_dict[(startStation, endStation)][1]
         return avg
-        
-        
-        
-
-        
-        
-        
-        
-#         checkin_dict {id:(stationName, t)}
-#         avg_time {(start, end):(total time, count)}
-        
-        
-#         45: ('Leyton', 3) 32: (Paradise, 8) 27:(Leyton, 10)
-                    
-                    
-#         45:(Waterloo, 15)
-            
-        
-#         (Leyton, Waterloo):(12, 1)
-            
-        
         
         
         
