225d86ce1a25bdf863082aa3b414869197003396
Rashmi Ananth <66443393+rashmi-ananth@users.noreply.github.com>
2022-09-21 19:08:01 -0400

    Time: 640 ms (81.97%), Space: 21.5 MB (73.34%) - LeetHub

--- 1229-meeting-scheduler/1229-meeting-scheduler.py
+++ 1229-meeting-scheduler/1229-meeting-scheduler.py
@@ -2,35 +2,33 @@ class Solution:
     def minAvailableDuration(self, slots1: List[List[int]], slots2: List[List[int]], duration: int) -> List[int]:
         
         
+        
+        
         slots1.sort()
         slots2.sort()
         
-        ptr1 = 0
-        ptr2 = 0
-        
-# [[10,50],[60,120],[140,210]]
-# [[0,15],[60,70]]
+        ptr1, ptr2 = 0, 0
         
         while ptr1 < len(slots1) and ptr2 < len(slots2):
-            start = max(slots1[ptr1][0], slots2[ptr2][0])
-            end = min(slots1[ptr1][1], slots2[ptr2][1])
             
-            if end - start >= duration:
-                return [start,start + duration]
+            if min(slots1[ptr1][1], slots2[ptr2][1]) - max(slots1[ptr1][0], slots2[ptr2][0]) >= duration:
+                return [max(slots1[ptr1][0], slots2[ptr2][0]), max(slots1[ptr1][0], slots2[ptr2][0]) + duration]
             
-
-
-            if slots1[ptr1][1] < slots2[ptr2][1]:
-                ptr1 += 1
-
+            elif slots1[ptr1][1] > slots2[ptr2][1]:
+                    ptr2 += 1
+                    
             else:
-                ptr2 += 1  
+                ptr1 += 1
                 
+        
         return []
-                
-                        
-                        
-                        
-                        
             
+            
+        
+        
+#         [10,50],[60,120],[140,210]
+#         [0,15],[60,70] 
+        
+        
+#         min(end) - max(start) >= duration
         
