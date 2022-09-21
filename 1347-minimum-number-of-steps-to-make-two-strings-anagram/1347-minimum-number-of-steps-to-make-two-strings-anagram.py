ea67ee93a77423771bc3a762bbe38245a30e7110
Rashmi Ananth <66443393+rashmi-ananth@users.noreply.github.com>
2022-09-21 15:27:01 -0400

    Time: 231 ms (71.18%), Space: 14.6 MB (26.64%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -1,22 +1,52 @@
-import numpy as np
 class Solution:
     def minSteps(self, s: str, t: str) -> int:
         
         
-        s_counter = Counter(s)
-        t_counter = Counter(t)
+        s_dict = defaultdict(int)
+        t_dict = defaultdict(int)
+        
+        for i in range(len(s)):
+            s_dict[s[i]] += 1
+            t_dict[t[i]] += 1
+        
+        count = 0
+        for char in s_dict.keys():
+            if char not in t_dict.keys():
+                count += s_dict[char]
+            else:
+                if s_dict[char] > t_dict[char]:
+                    count += s_dict[char] - t_dict[char]
+        return count
+                    
+                
+        
+        abb
+        aab
+        
+        
+        
+#         aab
+#         abb
+        
+#         a:2 b:1
+#         a:1 b:2
+        
+        
+#     l:1 e:3 t:1 c:1 o:1 d:1
+                            
+#     p:1 r:1 a:1 c:2 t:1 i:1 e:1
+                                
+                                
+                                
+#     1 + 2 + 0 + 0 + 1 + 1
     
-        
-        answer = 0
-        for i in s_counter.keys():
-            
-            if i not in t_counter.keys():
-                answer += s_counter[i]
-            elif s_counter[i] - t_counter[i] > 0:
-                answer += s_counter[i] - t_counter[i]
-                
-        return answer
-                
+#     total = 0
+#     if s[key] not in t.keys:
+#         total += s[key]
+#     else:
+#         if s[key] > t[key]:
+#             total += s[key] - t[key]
         
         
-     
+        
+        
