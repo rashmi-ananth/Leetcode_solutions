a44a1b756d8312cbf87e9d1e0662f6f5b76cd716
Rashmi Ananth <66443393+rashmi-ananth@users.noreply.github.com>
2022-09-21 18:34:30 -0400

    Time: 587 ms (16.37%), Space: 25.6 MB (10.51%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.py
+++ 1396-design-underground-system/1396-design-underground-system.py
@@ -1,81 +1,57 @@
 class UndergroundSystem:
 
     def __init__(self):
-        self.check_in_data = dict()
-        self.store_journey = dict()
+        
+        self.check_in_dict = dict()
+        self.trips_dict = dict()
         
 
     def checkIn(self, id: int, stationName: str, t: int) -> None:
-        self.check_in_data[id] = (stationName, t)
-        
+        self.check_in_dict[id] = (stationName, t)
 
     def checkOut(self, id: int, stationName: str, t: int) -> None:
-        difference = t - self.check_in_data[id][1]
-        if (self.check_in_data[id][0], stationName) in self.store_journey:
-            self.store_journey[(self.check_in_data[id][0], stationName)][0] += difference
-            self.store_journey[(self.check_in_data[id][0], stationName)][1] += 1
+        start_station = self.check_in_dict[id][0]
+        total_time = t - self.check_in_dict[id][1]
+        if (start_station, stationName) in self.trips_dict.keys():
+            self.trips_dict[(start_station, stationName)] = (self.trips_dict[(start_station, stationName)][0] + total_time, 1 + self.trips_dict[(start_station, stationName)][1])
+            
         else:
-            self.store_journey[(self.check_in_data[id][0], stationName)] = [difference, 1]
-
-
+            self.trips_dict[(start_station, stationName)] = (total_time, 1)
+            
         
 
     def getAverageTime(self, startStation: str, endStation: str) -> float:
-
-        return self.store_journey[(startStation, endStation)][0] / self.store_journey[(startStation, endStation)][1]
+        
+        avg = self.trips_dict[(startStation, endStation)][0] / self.trips_dict[(startStation, endStation)][1]
+        return avg
         
         
+        
 
+        
+        
+        
+        
+#         checkin_dict {id:(stationName, t)}
+#         avg_time {(start, end):(total time, count)}
+        
+        
+#         45: ('Leyton', 3) 32: (Paradise, 8) 27:(Leyton, 10)
+                    
+                    
+#         45:(Waterloo, 15)
+            
+        
+#         (Leyton, Waterloo):(12, 1)
+            
+        
+        
+        
+        
+        
 
 # Your UndergroundSystem object will be instantiated and called as such:
 # obj = UndergroundSystem()
 # obj.checkIn(id,stationName,t)
 # obj.checkOut(id,stationName,t)
 # param_3 = obj.getAverageTime(startStation,endStation)
-
-
-
-
-
-# checkin:
-    
-#     45: ("leyton", 3)
-#     32: ("paradise", 8)
-#     27: ("Leyton", 10)
-    
-    
-    
-    
-# checkout:
-    
-#     45: ("Waterloo", 15)
-#     27: ("Waterloo", 20)
-#     32: ("Cambridge", 22)
-    
-    
-    
-# avg:
-    
-#     ("Leyton", "Waterloo"): (total=22, count=2)
-#     ("Paradise", "Cambridge"): (total=14, count=1)
-    
-    
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
