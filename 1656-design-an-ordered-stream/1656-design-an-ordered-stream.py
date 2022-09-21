a11e9d26af5681ffcc70517e5c88cacb8d66550c
Rashmi Ananth <66443393+rashmi-ananth@users.noreply.github.com>
2022-09-20 20:26:30 -0400

    Time: 542 ms (13.83%), Space: 14.8 MB (47.49%) - LeetHub

--- 1656-design-an-ordered-stream/1656-design-an-ordered-stream.py
+++ 1656-design-an-ordered-stream/1656-design-an-ordered-stream.py
@@ -1,47 +1,23 @@
-class Node:
-    def __init__(self, id, val, next):
-        self.id = id
-        self.val = val
-        self.next = next
-    
 class OrderedStream:
 
     def __init__(self, n: int):
-        self.dummy = Node(0, "dummy", None)
-        self.ptr = 1
+        self.idx = 1
+        self.dictionary = dict()
+        
 
     def insert(self, idKey: int, value: str) -> List[str]:
+        self.dictionary[idKey] = value
+        return_lst = []
         
-        curr1 = self.dummy
-        curr2 = self.dummy.next
-        
-        while curr2 and curr2.id < idKey:
-            curr1 = curr2
-            curr2 = curr2.next
-        
-        curr1.next = Node(idKey, value, curr2)
-        
-        result = []
-        
-        while self.dummy.next and self.dummy.next.id == self.ptr:
-            result.append(self.dummy.next.val)
-            self.dummy.next = self.dummy.next.next
-            self.ptr += 1
+        while self.idx in self.dictionary.keys():
+            return_lst.append(self.dictionary[self.idx])
+            self.idx += 1
             
-            
-  
-        
-        return result
-        
-        
-            
-        
-        
-        
-        
+        return return_lst
         
 
 
 # Your OrderedStream object will be instantiated and called as such:
 # obj = OrderedStream(n)
 # param_1 = obj.insert(idKey,value)
+
