5e6bf5b9a2027bbd7f73fb1585f2e26e60f0419d
Rashmi Ananth <66443393+rashmi-ananth@users.noreply.github.com>
2022-09-22 10:30:26 -0400

    Time: 3751 ms (5.36%), Space: 52.8 MB (60.85%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.py
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.py
@@ -2,17 +2,51 @@ class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
         
         
-        sorted_intervals = sorted(intervals, key = lambda x:(x[1]))
+        intervals.sort()
+        print(intervals)
         
-        
-        end = sorted_intervals[0][1]
-        count = 1
-        for i in range(1, len(sorted_intervals)):
-            if sorted_intervals[i][0] >= end:
-                end = sorted_intervals[i][1]
-                count += 1
+#         ----------
+#             ----------
+            
+#         -----
+#                 -------
+            
+            
+#         ---------------
+#             ------
+
+
+
+
+            
+            
+        prev = intervals[0]
+        counter = 0
+        for i in range(1, len(intervals)):
+            if intervals[i][0] >= prev[1]:
+                prev = intervals[i]
                 
-        return len(sorted_intervals) - count
+            else:
+              #  print(intervals[i], prev)
+                if intervals[i][0] >= prev[0] and intervals[i][1] < prev[1]:
+                    prev = intervals[i]
+                
+                counter += 1
+                
+        return counter       
+        
+        
+#         sorted_intervals = sorted(intervals, key = lambda x:(x[1]))
+        
+        
+#         end = sorted_intervals[0][1]
+#         count = 1
+#         for i in range(1, len(sorted_intervals)):
+#             if sorted_intervals[i][0] >= end:
+#                 end = sorted_intervals[i][1]
+#                 count += 1
+                
+#         return len(sorted_intervals) - count
         
 
         
