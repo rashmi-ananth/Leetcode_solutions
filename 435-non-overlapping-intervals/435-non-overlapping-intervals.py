50d5c935c0f8e38e44374d328ed6f19e1366c290
Rashmi Ananth <66443393+rashmi-ananth@users.noreply.github.com>
2022-09-22 10:31:07 -0400

    Time: 2405 ms (45.63%), Space: 52.8 MB (60.85%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.py
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.py
@@ -4,21 +4,7 @@ class Solution:
         
         intervals.sort()
         print(intervals)
-        
-#         ----------
-#             ----------
-            
-#         -----
-#                 -------
-            
-            
-#         ---------------
-#             ------
-
-
-
-
-            
+    
             
         prev = intervals[0]
         counter = 0
@@ -27,32 +13,10 @@ class Solution:
                 prev = intervals[i]
                 
             else:
-              #  print(intervals[i], prev)
-                if intervals[i][0] >= prev[0] and intervals[i][1] < prev[1]:
+                if intervals[i][0] > prev[0] and intervals[i][1] < prev[1]:
                     prev = intervals[i]
                 
                 counter += 1
                 
         return counter       
-        
-        
-#         sorted_intervals = sorted(intervals, key = lambda x:(x[1]))
-        
-        
-#         end = sorted_intervals[0][1]
-#         count = 1
-#         for i in range(1, len(sorted_intervals)):
-#             if sorted_intervals[i][0] >= end:
-#                 end = sorted_intervals[i][1]
-#                 count += 1
-                
-#         return len(sorted_intervals) - count
-        
-
-        
-        
-        
-        
-        
-        
         
