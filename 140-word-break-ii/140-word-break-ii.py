17b1e54fda33fe291748cccfaf2deb9bf44757a0
Rashmi Ananth <66443393+rashmi-ananth@users.noreply.github.com>
2022-09-23 06:54:56 -0400

    Time: 33 ms (91.04%), Space: 13.9 MB (33.92%) - LeetHub

--- 140-word-break-ii/140-word-break-ii.py
+++ 140-word-break-ii/140-word-break-ii.py
@@ -4,30 +4,19 @@ class Solution:
         arr = [None] * (len(s) + 1)
         arr[0] = [[]]
         wordSet = set(wordDict)
-        # print(len(arr[0]), len(arr[1]))
-        
-      #  [[cats, and], [cat, sand]], 
-        
-        
-        print(arr)
         
         for i in range(len(s) + 1):
-            for word in wordSet:
-               # print(s[i], word, s[i: i + len(word)], word == s[i: i + len(word)])
-                if i + len(word) <= len(s) and s[i: i + len(word)] == word:
-                    # print(word)
-                    
-                    if arr[i] == None:
-                        continue
-                    for k in arr[i]:
-                        if arr[i+len(word)] == None:
-                            arr[i+len(word)] = []
-                        arr[i+len(word)].append(k + [word])
+            if arr[i] != None:
+                for word in wordSet:
+                    if i + len(word) <= len(s) and s[i: i + len(word)] == word:
+                        for k in arr[i]:
+                            if arr[i+len(word)] == None:
+                                arr[i+len(word)] = []
+                            arr[i+len(word)].append(k + [word])
         
         if arr[-1] == None:
             return []
         return_arr = []
-        print(arr)
         for i in arr[-1]:
             return_arr.append(' '.join(i))
         
