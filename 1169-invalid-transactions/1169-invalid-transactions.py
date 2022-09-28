c8be1d8e5a36b1f2e4ec2d7b41ecb3aebaa5188b
Rashmi Ananth <66443393+rashmi-ananth@users.noreply.github.com>
2022-09-27 22:28:06 -0400

    Time: 157 ms (46.28%), Space: 14.5 MB (73.78%) - LeetHub

--- 1169-invalid-transactions/1169-invalid-transactions.py
+++ 1169-invalid-transactions/1169-invalid-transactions.py
@@ -1,34 +1,59 @@
 class Solution:
     def invalidTransactions(self, transactions: List[str]) -> List[str]:
         
-        names = defaultdict(list)
-        arr = []
-        invalid = set()
-        for i in transactions:
-            arr.append(i.split(','))
         
-        for i in range(len(arr)):
-            if int(arr[i][2]) > 1000:
-                invalid.add(','.join(arr[i]))
-                
-            city = arr[i][-1]
-            time = int(arr[i][1])
-            for j in range(len(names[arr[i][0]])):
-                if city != names[arr[i][0]][j][-1] and abs(time - int(names[arr[i][0]][j][1])) <= 60:
-                    
-                    invalid.add(','.join(arr[i]))
-                    invalid.add(','.join(names[arr[i][0]][j]))
-                    
-                
-                
-            names[arr[i][0]].append(arr[i])
-         
-        return_lst = []
+#     dict: name:[(idx, [t, city]), (idx, [t, city])]
         
-        for i in transactions:
-            if i in invalid:
-                return_lst.append(i)
+#         set: 0,2,5 - result idx
+
+        
+        result_idx = set()
+        visited_dictionary = defaultdict(list)
+        
+        for i in range(len(transactions)):
+            name, t, amt, city = transactions[i].split(',')
+            
+            if int(amt) > 1000:
+                result_idx.add(i)
                 
-        return return_lst
+            for item in visited_dictionary[name]:
+                t2 = item[1][0]
+                city2 = item[1][1]
+                
+                if city != city2 and abs(int(t)-t2) <= 60:
+                    result_idx.add(i)
+                    result_idx.add(item[0])
+            visited_dictionary[name].append((i,[int(t), city]))
+            
+            
+        
+        result = []
+        for i in result_idx:
+            result.append(transactions[i])
+    
+        return result
+            
+            
+        
+            
+                    
+                    
+            
+                
                 
             
+            
+            
+            
+            
+
+        
+
+
+
+            
+            
+                
+        
+        
+        
