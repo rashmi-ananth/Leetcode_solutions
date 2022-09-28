3ed54e98217617c78a9f267b5ffe173f171898b7
Rashmi Ananth <66443393+rashmi-ananth@users.noreply.github.com>
2022-09-28 14:57:27 -0400

    Time: 2036 ms (95.21%), Space: 14 MB (50.77%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -1,33 +1,30 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        
-        directions = [(0,1), (0,-1), (1,0), (-1,0)]
-        
-        def dfs(board, i, j, visited, k):
-            
-            if k >= len(word):
-                return True
-           
-            if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]) or (i,j) in visited:
-                return False
 
+        row, col = len(board), len(board[0])
+        path = set()
+        s = ''
+        for r in range(row):
+            for c in range(col):
+                s += board[r][c]
+        for w in word:
+            if w not in s:
+                return False
+        def dfs(r, c, i):
+            if r < 0 or r >= row or c < 0 or c >= col or board[r][c] != word[i] or (r, c) in path:
+                return False
+            if board[r][c] == word[i] and i == len(word) - 1:
+                return True
             
-            if board[i][j] == word[k]:
-                for x,y in directions:
-                    visited.add((i, j))
-                    if dfs(board, i+x, j+y, visited, k+1):
-                        return True
-                    visited.remove((i, j))
-            return False  
-   
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if board[i][j] == word[0]:
-                    if dfs(board, i, j, set(), 0):
-                        return True
-                    
-        return False
-                    
-                    
-                    
+            path.add((r, c))
+            res = (dfs(r+1, c, i+1) or dfs(r-1, c, i+1) 
+                   or dfs(r, c+1, i+1) or dfs(r, c-1, i+1))
+            path.remove((r, c))
+            
+            return res
         
+        for r in range(row):
+            for c in range(col):
+                if dfs(r, c, 0):
+                    return True
+        return False
