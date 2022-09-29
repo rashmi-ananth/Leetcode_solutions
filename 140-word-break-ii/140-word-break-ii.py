172d4928a08d304d42183915b8724a4eb571d5ba
Rashmi Ananth <66443393+rashmi-ananth@users.noreply.github.com>
2022-09-29 09:03:23 -0400

    Time: 59 ms (27.00%), Space: 13.8 MB (81.98%) - LeetHub

--- 140-word-break-ii/140-word-break-ii.py
+++ 140-word-break-ii/140-word-break-ii.py
@@ -1,27 +1,26 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
         
-        arr = [None] * (len(s) + 1)
-        arr[0] = [[]]
         
-        for i in range(len(s) + 1):
+        arr = [None] * (len(s) + 1)
+        arr[0] = [""]
+        
+        
+        for i in range(len(s)):
             if arr[i] != None:
                 for word in wordDict:
-                    if i + len(word) <= len(s) and s[i: i + len(word)] == word:
-                        for k in arr[i]:
-                            if arr[i+len(word)] == None:
-                                arr[i+len(word)] = []
-                            arr[i+len(word)].append(k + [word])
+                    if word == s[i:i+len(word)]:
+                        if arr[i+len(word)] == None:
+                            arr[i+len(word)] = []
+                        
+                        for sub in arr[i]:
+                            arr[i+len(word)].append((sub + " " + word).strip())
         
-        if arr[-1] == None:
-            return []
-        return_arr = []
-        for i in arr[-1]:
-            return_arr.append(' '.join(i))
-        
-        return  return_arr
+        return arr[-1]
         
         
+        
+         
         
         
         
