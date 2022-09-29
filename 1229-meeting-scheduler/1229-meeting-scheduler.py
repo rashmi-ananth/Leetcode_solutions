4a3d385cc56baf1b96306c666f2fd1ee7fac62ec
Rashmi Ananth <66443393+rashmi-ananth@users.noreply.github.com>
2022-09-28 21:38:30 -0400

    Time: 1513 ms (8.27%), Space: 21.6 MB (72.90%) - LeetHub

--- 1229-meeting-scheduler/1229-meeting-scheduler.py
+++ 1229-meeting-scheduler/1229-meeting-scheduler.py
@@ -2,24 +2,28 @@ class Solution:
     def minAvailableDuration(self, slots1: List[List[int]], slots2: List[List[int]], duration: int) -> List[int]:
         
         
-        # O(N + M)
+        # sort both slots based on first id
+        # compare max(start) min(end) > dur
+        # return
+        # else update ptr to one that ends earlier
         
         slots1.sort()
         slots2.sort()
-        
-        ptr1, ptr2 = 0, 0
+        ptr1 = 0
+        ptr2 = 0
         
         while ptr1 < len(slots1) and ptr2 < len(slots2):
+            start = max(slots1[ptr1][0], slots2[ptr2][0])
+            end = min(slots1[ptr1][1], slots2[ptr2][1])
+            if end - start >= duration:
+                return [start, start + duration]
             
-            if min(slots1[ptr1][1], slots2[ptr2][1]) - max(slots1[ptr1][0], slots2[ptr2][0]) >= duration:
-                return [max(slots1[ptr1][0], slots2[ptr2][0]), max(slots1[ptr1][0], slots2[ptr2][0]) + duration]
-            
-            elif slots1[ptr1][1] > slots2[ptr2][1]:
-                    ptr2 += 1
-                    
             else:
-                ptr1 += 1
-                
-        
+                if slots2[ptr2][1] < slots1[ptr1][1]:
+                    ptr2 += 1
+                else:
+                    ptr1 += 1
+             
         return []
-            
+        
+        
