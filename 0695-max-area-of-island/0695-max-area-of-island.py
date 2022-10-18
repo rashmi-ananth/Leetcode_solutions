191dadc6f6f5f7a02b081fc9d304f42036292e2d
Rashmi Ananth <66443393+rashmi-ananth@users.noreply.github.com>
2022-10-18 13:07:32 -0400

    Time: 586 ms (5.07%), Space: 17 MB (42.45%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -5,7 +5,6 @@ class Solution:
         
         directions = [(1,0), (0,1), (-1,0), (0,-1)]
         def dfs(i, j, count, visited):
-            # print(i,j, count, visited)
             if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]):
                 return count
             
@@ -22,20 +21,16 @@ class Solution:
             max_count = count
             for x,y in directions:
                 max_count = max(max_count, dfs(i+x, j+y, max_count, visited))
-     
-               
+
                 
             return max_count
             
-        
-        
+
         
         max_count = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == 1:
-                    # print("STARTT")
-                    # print(i,j)
                     count = dfs(i, j, 0, set())
 
                     max_count = max(max_count, count)
