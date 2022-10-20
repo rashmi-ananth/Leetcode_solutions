147a99e334696dfdc07b839126798a14e1741464
Rashmi Ananth <66443393+rashmi-ananth@users.noreply.github.com>
2022-10-20 09:50:46 -0400

    Time: 1206 ms (41.71%), Space: 49.5 MB (5.91%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
@@ -1,21 +1,20 @@
 class Solution:
     def makeConnected(self, n: int, connections: List[List[int]]) -> int:
         
-        # Time: O(M + N)
-        # Space: O(M + N)
-        
-        if len(connections) < n-1:
+        if len(connections) < n - 1:
             return -1
         
-        def dfs(i, visted, adj_lst):
+        def dfs(i, adj_lst):
             
             if i in visited:
                 return
+            
             visited.add(i)
             for j in adj_lst[i]:
-                dfs(j, visited, adj_lst)
+                dfs(j, adj_lst)
             
-  
+
+        
         adj_lst = dict()
         for i in range(n):
             adj_lst[i] = []
@@ -23,15 +22,17 @@ class Solution:
         for i, j in connections:
             adj_lst[i].append(j)
             adj_lst[j].append(i)
-            
+         
         visited = set()
         count = 0
-        for i in adj_lst.keys():
+        for i in range(n):
             if i not in visited:
-                dfs(i, visited, adj_lst)
+                dfs(i, adj_lst)
                 count += 1
-
+        
         return count - 1
-        
-                
-        
+            
+            
+            
+            
+            
