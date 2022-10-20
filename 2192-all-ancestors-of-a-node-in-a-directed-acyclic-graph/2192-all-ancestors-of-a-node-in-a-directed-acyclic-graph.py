95063c6bfde0e4f4a23baf478bab5f6cd38d1560
Rashmi Ananth <66443393+rashmi-ananth@users.noreply.github.com>
2022-10-20 10:17:35 -0400

    Time: 1135 ms (77.57%), Space: 31.4 MB (82.64%) - LeetHub

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
@@ -3,18 +3,17 @@ class Solution:
         
         
         def dfs(i, visited, adj_lst):
-            
             if i in visited.keys():
                 return visited[i]
             
-     
             for j in adj_lst[i]:
                 visited[i] += [j] + dfs(j, visited, adj_lst)
-            visited[i] = list(set(visited[i]))
                 
+            visited[i] = list(set(visited[i]))
+            
             return visited[i]
-
         
+  
         adj_lst = dict()
         for i in range(n):
             adj_lst[i] = []
@@ -22,23 +21,20 @@ class Solution:
         for i, j in edges:
             adj_lst[j].append(i)
             
+        
         visited = defaultdict(list)
-        for i in range(n):
-            lst = dfs(i, visited, adj_lst)
-
+        
         return_lst = []
-        sorted_keys = sorted(visited)
-        
-        for key in sorted_keys:
-            visited[key].sort()
-            return_lst.append(visited[key])
-            
+        for i in range(n):
+            if i not in visited:
+                dfs(i, visited, adj_lst)
+         
+        for i in range(n):
+            return_lst.append(sorted(visited[i]))
+                
+         
         return return_lst
-            
-        
-        
-        
-        
-        
-        
+                
+                
+                
         
