cbab9cca00de9d3131d7269e161f5009de2c3eef
Rashmi Ananth <66443393+rashmi-ananth@users.noreply.github.com>
2022-10-28 15:52:44 -0400

    Time: 93 ms (80.07%), Space: 14 MB (50.13%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -15,19 +15,10 @@ class Solution:
             else:
                 
                 start = dictionary[s[i]] + 1
-                max_length = max(max_length, curr_len)
+                
                 curr_len = 0
                 dictionary[s[i]] = i
-                
-                
+                        
                 
         max_length = max(max_length, curr_len)
         return max_length
-    
-    
-    # "aegrhtsry etrwaesnr"
-    # start = 4
-    # 5
-    # 7
-    # {a:0, e:9, g:2, r:7, h:4, t:5 s:6, " ":8}
-        
