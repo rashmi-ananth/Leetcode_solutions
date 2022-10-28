5c0771e4bc8ef44c7ab982974e437bc634c9fe6b
Rashmi Ananth <66443393+rashmi-ananth@users.noreply.github.com>
2022-10-28 14:56:23 -0400

    Time: 293 ms (80.93%), Space: 25.2 MB (37.29%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.py
+++ 1396-design-underground-system/1396-design-underground-system.py
@@ -2,51 +2,29 @@ class UndergroundSystem:
 
     def __init__(self):
         self.check_in_dict = dict()
-        self.paths = dict()
+        self.total_dict = dict()
         
 
     def checkIn(self, id: int, stationName: str, t: int) -> None:
-        self.check_in_dict[id] = [stationName, t]
+        self.check_in_dict[id] = (stationName, t)
         
 
     def checkOut(self, id: int, stationName: str, t: int) -> None:
-        start = self.check_in_dict[id][0]
-        start_time = self.check_in_dict[id][1]
-        difference = t - start_time
-        
-        if (start, stationName) in self.paths.keys():
-            self.paths[(start, stationName)][0] += difference
-            self.paths[(start, stationName)][1] += 1
+        start_station, start_time = self.check_in_dict[id]
+        diff = t - start_time
+        if (start_station, stationName) in self.total_dict.keys():
+            self.total_dict[(start_station, stationName)][0] += diff
+            self.total_dict[(start_station, stationName)][1] += 1
         else:
-            self.paths[(start, stationName)] = [difference, 1]
-            
+            self.total_dict[(start_station, stationName)] = [diff, 1]
+                
+        
+        
 
     def getAverageTime(self, startStation: str, endStation: str) -> float:
-        total, count = self.paths[(startStation, endStation)]
-        return total / count
-        
+        return self.total_dict[(startStation, endStation)][0] / self.total_dict[(startStation, endStation)][1]
         
 
- 
-
-
-
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
 
 # Your UndergroundSystem object will be instantiated and called as such:
 # obj = UndergroundSystem()
