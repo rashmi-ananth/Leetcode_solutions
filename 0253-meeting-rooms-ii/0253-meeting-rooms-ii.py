841cdee6161eb827127fa54acf5435d055e49570
Rashmi Ananth <66443393+rashmi-ananth@users.noreply.github.com>
2022-10-28 15:20:33 -0400

    Time: 183 ms (33.52%), Space: 17.4 MB (83.68%) - LeetHub

---
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -0,0 +1,24 @@
+from heapq import heapify, heappush, heappop
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        
+        count = 1
+        intervals.sort()
+        h = [intervals[0][1]]
+        heapify(h)
+        
+        
+        for i in range(1, len(intervals)):
+            if intervals[i][0] < h[0]:
+                count += 1    
+            else:
+                heappop(h)
+            heappush(h, intervals[i][1])
+                
+        return count
+            
+        
+        
+        
+        
