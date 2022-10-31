36df33fe1faf8e1c32fc582d0aefa4c915396ddc
Rashmi Ananth <66443393+rashmi-ananth@users.noreply.github.com>
2022-10-31 17:59:24 -0400

    Time: 134 ms (24.52%), Space: 14 MB (25.24%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -0,0 +1,19 @@
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        
+        freq = defaultdict(int)
+        
+        for i in range(len(words)):
+            freq[words[i]] += 1
+          
+        
+            
+        result = sorted(freq.items(), key = lambda x: (-x[1], x[0]))
+        
+        return_lst = []
+        for i in range(k):
+            return_lst.append(result[i][0])
+        
+        return return_lst
+        
+        
