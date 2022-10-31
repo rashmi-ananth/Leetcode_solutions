1d2701f2a6106727488a936d7e16d51c472fb59d
Rashmi Ananth <66443393+rashmi-ananth@users.noreply.github.com>
2022-10-31 17:32:53 -0400

    Time: 73 ms (95.08%), Space: 14.3 MB (71.30%) - LeetHub

--- 1244-design-a-leaderboard/1244-design-a-leaderboard.py
+++ 1244-design-a-leaderboard/1244-design-a-leaderboard.py
@@ -2,42 +2,27 @@ from heapq import heapify, heappush, heappop
 class Leaderboard:
 
     def __init__(self):
-        self.dictionary = defaultdict(int)
+        self.lb = defaultdict(int)
         
 
     def addScore(self, playerId: int, score: int) -> None:
-        if playerId in self.dictionary.keys():
-            self.dictionary[playerId] += score
-        else:
-            self.dictionary[playerId] = score
-            
+        self.lb[playerId] += score
+
     def top(self, K: int) -> int:
-        
         h = []
-        dictionary_items = list(self.dictionary.items())
-        i = 0
-        while i < K:
-            heappush(h, (dictionary_items[i][1], dictionary_items[i][0]))
-            i += 1
-            
-        while i < len(dictionary_items):
-            if dictionary_items[i][1] > h[0][0]:
-                heappop(h)
-                heappush(h, (dictionary_items[i][1], dictionary_items[i][0]))
-            i += 1
-         
+        heapify(h)
+        
+        for v in self.lb.values():
+            heappush(h, -v)
         
         total = 0
-        for i in range(len(h)):
-            total += h[i][0]
+        for i in range(K):
+            total += - heappop(h)
+        
         return total
-            
-    
 
     def reset(self, playerId: int) -> None:
-        
-        self.dictionary[playerId] = 0
-        del self.dictionary[playerId]
+        self.lb[playerId] = 0
         
 
 
