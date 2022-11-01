9d46664f2ecb78fecb14639af26f7bd491a48de5
Rashmi Ananth <66443393+rashmi-ananth@users.noreply.github.com>
2022-11-01 15:29:38 -0400

    Time: 50 ms (65.89%), Space: 13.9 MB (19.71%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.py
@@ -0,0 +1,53 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        
+        
+        # 2[abc]3[cd]ef
+        # [2 1 ]
+        # abc
+        
+        stack = []
+        i = 0
+        while i < len(s):
+            if s[i] != ']':
+                stack.append(s[i])
+                i += 1
+                continue
+            
+            string_lst = []
+            char = 'a'
+            while char.isalpha():
+                char = stack.pop()
+                if char.isalpha():
+                    string_lst.append(char)
+                
+            string_lst = string_lst[::-1]
+            string = ''.join(string_lst)
+            
+            integer = 0
+            factor = 1
+            while True:
+                if stack and stack[-1].isdigit():
+                    integer += factor * int(stack.pop())
+                    factor *= 10
+                else:
+                    break
+                
+            final_str = integer * string
+            stack.append(final_str)
+            i += 1
+
+            
+        return ''.join(stack)
+            
+         
+         
+         
+         
+         
+            
+            
+        
+        
+       
+        
