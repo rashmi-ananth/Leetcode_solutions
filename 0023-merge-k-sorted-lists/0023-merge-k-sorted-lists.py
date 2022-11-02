16d3c2e15435d9d06794559b81059d01fec27197
Rashmi Ananth <66443393+rashmi-ananth@users.noreply.github.com>
2022-11-02 13:11:23 -0400

    Time: 239 ms (44.60%), Space: 17.7 MB (74.55%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -1,3 +1,4 @@
+from heapq import heapify, heappush, heappop
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -6,19 +7,45 @@
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         
-        lst = []
-        for i in range(len(lists)):
-            curr = lists[i]
-            while curr != None:
-                lst.append(curr.val)
-                curr = curr.next
-                
-        lst.sort()
-        dummy = ListNode()
-        curr = dummy
-        for i in range(len(lst)):
-            curr.next = ListNode(lst[i])
-            curr = curr.next
+        if len(lists) == 0:
+            return None
         
+        h = []
+        heapify(h)
+        counters = [None] * len(lists)
+        
+        dummy = ListNode()
+        for i in range(len(lists)):
+            counters[i] = lists[i]
+            if lists[i] != None:
+                heappush(h, (lists[i].val, i))
+
+        
+        curr = dummy
+        while len(h) > 0:
+            val, idx = heappop(h)
+            curr.next = counters[idx]
+            counters[idx] = counters[idx].next
+            if counters[idx] != None:
+                heappush(h, (counters[idx].val, idx))
+            curr = curr.next
+
         return dummy.next
+        
+        
+#         lst = []
+#         for i in range(len(lists)):
+#             curr = lists[i]
+#             while curr != None:
+#                 lst.append(curr.val)
+#                 curr = curr.next
+                
+#         lst.sort()
+#         dummy = ListNode()
+#         curr = dummy
+#         for i in range(len(lst)):
+#             curr.next = ListNode(lst[i])
+#             curr = curr.next
+        
+#         return dummy.next
         
