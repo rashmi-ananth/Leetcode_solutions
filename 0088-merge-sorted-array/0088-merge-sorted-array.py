630b7474b21d4dbbc157c5d4a8b30ac49672d2b1
Rashmi Ananth <66443393+rashmi-ananth@users.noreply.github.com>
2022-11-03 01:30:49 -0400

    Time: 97 ms (5.13%), Space: 13.9 MB (85.83%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,11 +3,42 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        idx = len(nums1) - n
-        for i in range(len(nums2)):
-            nums1[idx] = nums2[i]
-            idx += 1
+        
+        nums1_copy = []
+        for i in range(m):
+            nums1_copy.append(nums1[i])
+        idx1 = 0
+        idx2 = 0
+        
+        
+        
+        for i in range(m+n):
+            # print(nums1, nums1_copy[idx1], nums2[idx2])
+            if idx1 < m and idx2 < n:
+                if nums1_copy[idx1] <= nums2[idx2]:
+                    nums1[i] = nums1_copy[idx1]
+                    idx1 += 1
+                else:
+                    nums1[i] = nums2[idx2]
+                    idx2 += 1
+            else:
+                break
+         
+        # 1 2 2 3 
+        
+        if idx1 < m:
+            while idx1 < m:
+                nums1[i] = nums1_copy[idx1]
+                idx1 += 1
+                i += 1
+               
 
-        nums1.sort()
+        if idx2 < n:
+            while idx2 < n:
+                nums1[i] = nums2[idx2]
+                idx2 += 1
+                i += 1
+
+
             
         
