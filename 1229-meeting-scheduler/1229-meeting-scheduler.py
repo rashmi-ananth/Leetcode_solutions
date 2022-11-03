1af93fe49b6332cb3a10e053f284c2c2111b16ad
Rashmi Ananth <66443393+rashmi-ananth@users.noreply.github.com>
2022-11-03 16:14:28 -0400

    Time: 727 ms (79.58%), Space: 21.7 MB (39.44%) - LeetHub

--- 1229-meeting-scheduler/1229-meeting-scheduler.py
+++ 1229-meeting-scheduler/1229-meeting-scheduler.py
@@ -1,29 +1,23 @@
 class Solution:
     def minAvailableDuration(self, slots1: List[List[int]], slots2: List[List[int]], duration: int) -> List[int]:
         
-        
-        # sort both slots based on first id
-        # compare max(start) min(end) > dur
-        # return
-        # else update ptr to one that ends earlier
-        
+        ptr1, ptr2 = 0, 0
         slots1.sort()
         slots2.sort()
-        ptr1 = 0
-        ptr2 = 0
         
         while ptr1 < len(slots1) and ptr2 < len(slots2):
-            start = max(slots1[ptr1][0], slots2[ptr2][0])
-            end = min(slots1[ptr1][1], slots2[ptr2][1])
-            if end - start >= duration:
-                return [start, start + duration]
+            max_start = max(slots1[ptr1][0], slots2[ptr2][0])
+            min_end = min(slots1[ptr1][1], slots2[ptr2][1])
+            if min_end - max_start >= duration:
+                return [max_start, max_start + duration]
             
+            if slots1[ptr1][1] > slots2[ptr2][1]:
+                ptr2 += 1
             else:
-                if slots2[ptr2][1] < slots1[ptr1][1]:
-                    ptr2 += 1
-                else:
-                    ptr1 += 1
-             
+                ptr1 += 1
+            
+            
         return []
+        
         
         
